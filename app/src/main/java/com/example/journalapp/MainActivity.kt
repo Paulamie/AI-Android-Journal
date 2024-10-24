22e0990c30b02836d1fbcd764dbc74c786b280fc
AhmadTNofal <atnofal04@gmail.com>
2024-10-24 01:02:40 +0100

    Innit

--- app/src/main/java/com/example/journalapp/MainActivity.kt
+++ app/src/main/java/com/example/journalapp/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.journalapp
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View  // Import for controlling view visibility
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
